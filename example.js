b7db793ea7d30c0581241d2c87cb691711b2aca3
TJ Holowaychuk <tj@vision-media.ca>
2013-09-09 08:23:34 -0700

    Initial commit

---
+++ example.js
@@ -0,0 +1,24 @@
+
+var rewrite = require('./');
+var koa = require('koa');
+
+var app = koa();
+
+// GET /i124
+app.use(rewrite(/^\/i(\w+)/, '/items/$1'));
+
+// GET /foo..bar
+//app.use(rewrite('/:src..:dst', '/commits/$1/to/$2'));
+app.use(rewrite('/:src..:dst', '/commits/:src/to/:dst'));
+
+// GET /js/jquery.js
+app.use(rewrite('/js/*', '/public/assets/js/$1'));
+
+app.use(function(next){
+  return function *(){
+    this.body = this.url + '\n';
+  }
+});
+
+app.listen(3000);
+console.log('listening on port 3000');
---
+++ index.js
@@ -0,0 +1,67 @@
+
+/**
+ * Module dependencies.
+ */
+
+ var debug = require('debug')('koa-rewrite');
+ var toRegexp = require('path-to-regexp');
+
+/**
+ * Expose `expose`.
+ */
+
+module.exports = rewrite;
+
+/**
+ * Rwrite `src` to `dst`.
+ *
+ * @param {String|RegExp} src
+ * @param {String} dst
+ * @return {Function}
+ * @api public
+ */
+
+function rewrite(src, dst) {
+  var keys = [];
+  var re = toRegexp(src, keys);
+  var map = toMap(keys);
+
+  debug('rewrite %s -> %s    %s', src, dst, re);
+
+  return function(next){
+    return function *(){
+      var orig = this.path;
+      var m = re.exec(orig);
+      
+      if (m) {
+        this.path = dst.replace(/\$(\d+)|(?::(\w+))/g, function(_, n, name){
+          if (name) return m[map[name].index + 1];
+          return m[n];
+        });
+
+        debug('rewrite %s -> %s', orig, this.path);
+      }
+
+      yield next;
+    }
+  }
+}
+
+/**
+ * Turn params array into a map for quick lookup.
+ *
+ * @param {Array} params
+ * @return {Object}
+ * @api private
+ */
+
+function toMap(params) {
+  var map = {};
+
+  params.forEach(function(param, i){
+    param.index = i;
+    map[param.name] = param;
+  });
+
+  return map;
+}
